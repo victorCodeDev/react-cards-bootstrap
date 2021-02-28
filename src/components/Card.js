64f6ced9b427447169690d36830eba9e2ac7009b
victor.ojeda <victor.ojeda@konecta.com.py>
2021-02-28 15:49:23 -0300

    Se termina de implementar los estilos para las tarjetas y que redireccionen a sitios de streaming

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,33 @@
-import React from 'react'
-import image1 from '../assets/image1.jpg'
+import React from 'react';
+import './card.css';
+import PropTypes from 'prop-types'
 
-function Card() {
+function Card({ title, imageSource, text, url }) {
     return (
-        <div className="card text-center bg-dark">
-            <div className="card-body text-light">
-                <img src={image1} alt="" />
-                <h4 className="card-title">Mi Titulo</h4>
+        <div className="card text-center bg-dark animate__animated animate__fadeInUp">
+            <div className="card-body text-light" >
+                <div className="overflow">
+                    <img src={imageSource} alt="Sitio Streamming" className="card-img-top img-height" />
+                </div>
+                <h4 className="card-title">{title}</h4>
                 <p className="card-text text-secondary">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Placeat voluptatem pariatur necessitatibus ad quia veritatis
-                    eveniet, ab ullam, adipisci error autem mollitia labore dicta
-                    nisi quidem neque optio nihil minus?
+                    {
+                        text ? text : 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.Placeat voluptatem pariatur necessitatibus ad quia veritatis eveniet, ab ullam, adipisci error autem mollitia labore dicta nisi quidem neque optio nihil minus?'
+                    }
+
                 </p>
-                <a href="#!" className="btn btn-outline-secondary rounded-0">Go to Website</a>
+                <a href={url} className="btn btn-outline-secondary rounded-0" target="_blank" rel="noreferrer">Ir al sitio</a>
             </div>
 
         </div>
     )
+}
+
+Card.propTypes = {
+    title: PropTypes.string.isRequired,
+    imageSource: PropTypes.string,
+    text: PropTypes.string,
+    url: PropTypes.string,
 }
 
 export default Card
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,19 +1,45 @@
 import React from 'react';
 import Card from './Card';
+import image1 from '../assets/image1.png';
+import image2 from '../assets/image2.png';
+import image3 from '../assets/image3.png';
+
+const cards = [
+    {
+        id: 1,
+        title: 'Netflix',
+        image: image1,
+        text: 'Disfruta de Netflix, películas y series en streaming en tu smart TV, consola, PC, Mac, móvil, tableta y más dispositivos.',
+        url: 'https://www.netflix.com',
+    },
+    {
+        id: 2,
+        title: 'Disney Plus',
+        image: image2,
+        text: 'Las mejores historias. Desde nuevos lanzamientos y Originales Disney+, hasta clásicos inolvidables. Sin Anuncios. Descargas Ilimitadas. 7 perfiles por cuenta. 4 pantallas simultáneas.',
+        url: 'https://www.disneyplus.com/es-py',
+    },
+    {
+        id: 3,
+        title: 'Amazon Prime',
+        image: image3,
+        text: 'Disfruta de Amazon Originals exclusivas, además de películas y series populares, por PYG 24 900/mes. Disfruta ahora, cancela cuando quieras.',
+        url: 'https://www.primevideo.com'
+    },
+]
 
 function Cards() {
     return (
         <div className="container d-flex justify-content-center h-100 align-items-center">
             <div className="row">
-                <div className="col-md-4">
-                    <Card />
-                </div>
-                <div className="col-md-4">
-                    <Card />
-                </div>
-                <div className="col-md-4">
-                    <Card />
-                </div>
+                {
+                    // eslint-disable-next-line
+                    cards.map((card) => (
+                        <div className="col-md-4 mb-3" key={card.id}>
+                            <Card title={card.title} imageSource={card.image} text={card.text} url={card.url} />
+                        </div>
+                    ))
+                }
             </div>
         </div>
     )
